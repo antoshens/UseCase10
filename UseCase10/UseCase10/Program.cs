cc9d47df7811af9d5607a62b41ac01aca04f5023
Anton Honcharenko <ahoncha@softserveinc.com>
2023-06-29 19:58:27 +0300

    UC#10 Implement regex

--- UseCase10/UseCase10/Program.cs
+++ UseCase10/UseCase10/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+static bool HasMatch(string input, int maxNumberOfChars)
+{
+    var regexExpression = @"^(?=.*[A-Z]){1,}(?=.*[a-z]){1}(?=.*\d{1})(?=.*[!""#$%&'()*+,\-./:;<=>?@\[\]^_`{|}~])[A-Za-z\d!""#$%&'()*+,\-./:;<=>?@\[\]^_`{|}~]{1," + maxNumberOfChars + "}$";
+    var regex = new Regex(regexExpression);
+    var isMatch = regex.IsMatch(input);
+
+    return isMatch;
+}
